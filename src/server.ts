60171d55f8360cc9cdf879da76986fecdf4be87c
Pedro Viana Araújo Silva <pedrovianaaraujosilva@gmail.com>
2021-02-24 00:45:40 -0300

    creating migration with sqlite databse

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,10 @@
+import 'reflect-metadata'
 import express from 'express';
+import './database'
+import {router} from './routes'
+
 const app = express();
 
-app.get('/', (req, res)=> {
-    return res.json({
-        mensagem: "servidor rodando"
-    })
-})
-app.post('/', (req, res)=> {
-    return res.json({
-        mensagem: "post funfando"
-    })
-})
+app.use(express.json())
+app.use(router)
 app.listen(3050, ()=> console.log("Server is running!"));
